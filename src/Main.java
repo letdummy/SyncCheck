1099f6b105922ab4b1e986bd8a70de7592c2f813
ags <71609913+letdummy@users.noreply.github.com>
2022-10-19 23:49:07 +0700

    adding code from windows

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Jadi ini code gw tulis di linux, btar bawah line ini gw tulis di windows");
+        System.out.println("Nah terus ini code gw tulis di windows, and viola..... jadi");
     }
 }
