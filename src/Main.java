5c9139111377e2d15330ee638c8d4e72a008719f
letdummy <71609913+letdummy@users.noreply.github.com>
2022-10-20 06:34:14 +0700

    adding code from linux

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Jadi ini code gw tulis di linux, btar bawah line ini gw tulis di windows");
     }
 }
